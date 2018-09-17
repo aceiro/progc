de91f2a25e882140f3c8702665b9ce41896751d6
gismarb <gismar_barbosa@yahoo.com.br>
2018-09-17 15:39:44 -0300

    Nível Básico - Exercício 1

--- exercicios/gismar/execercicio1.c
+++ exercicios/gismar/execercicio1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/* 1 -Faça um programa em C que imprima a mensagem “Hello, world!” */
+/*
+	1 -Faça um programa em C que imprima a mensagem “Hello, world!” 
+*/
 
 int main () {
 	
