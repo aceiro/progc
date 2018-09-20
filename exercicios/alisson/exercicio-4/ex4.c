dbfa55b37f4a46915b5e8ea7059357c80236f26f
AlunoMAQ315 <alisson.daniel123@hotmail.com>
2018-09-19 21:54:15 -0300

    Enviando lista inicial dos exercicios

---
+++ exercicios/alisson/exercicio-4/ex4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+int target = 10;
+if (target == 10) {
+printf("Target is equal to 10");
+}
+return 0;
+}
---
+++ exercicios/alisson/exercicio-5/ex5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int foo = 1;
+int bar = 2;
+if (foo > bar) {
+printf("Foo is bigger to bar");
+}
+if(bar > foo){
+printf("\n\nBar is bigger to foo\n\n");
+
+}
+return 0;
+}
