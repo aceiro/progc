0f92f26cc5f9c049831748be03148e38ab24b308
Amanda <amandasantos@outlook.com>
2018-09-26 20:14:15 -0300

    Correções Exercicios

--- exercicios/amanda/exercicio3.c
+++ exercicios/amanda/exercicio3.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#define MAX_LIMIT 3
 
 int main () {
 	/*TODO: defina a variavel grades*/
-	int average, grades[3];
+	int average, grades[MAX_LIMIT];
 
 	grades [0] = 80;
 	grades [1] = 85;
 	grades [2] = 90;
 
 	average = (grades[0] + grades[1] + grades[2]) / 3;
-	printf("The average of the 3 grades is %d", average );
+	printf("The average of the 3 grades is %d\n", average );
 }
