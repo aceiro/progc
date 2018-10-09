efc8660abb17e4d41956d495b8f99e37e2ff788c
Amanda Santos <amandasantos@outlook.com>
2018-10-09 16:30:20 -0300

    Exercicio 14

--- exercicios/amanda/ex14.c
+++ exercicios/amanda/ex14.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
-main (){
+main () {
 	printf("Exemplo 1 - For\n");
 	int i;
-	for (int i = 1; i <= 100 ; i++)
-	{
+	for(i=100 ; i<= 100 && i > 0 ; i--){
 		printf("%i\n", i );
 	}
 
 	printf("Exemplo 2 - While\n");
-	while (i <= 100) {
+	i = 100;
+	while(i<=100 && i > 0){
 		printf("%i\n", i);
-		i++;
+		i--;
 	}
 
 	printf("Exemplo 3 - Do While\n");
-	i = 0;
+	i = 100;
 	do {
 		printf("%i\n", i);
-		i++;
-	} while (i <= 100);
+		i--;
+	} while (i <=100 && i > 0);
 
 	return 0;
+
 }
