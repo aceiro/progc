45643c8a552a31b484ad9fa34a2dae08942e62d1
gismarb <gismar_barbosa@yahoo.com.br>
2018-10-12 22:28:08 -0300

    Prova - Exercicio 5

--- prova/gismar/prova_exec5.c
+++ prova/gismar/prova_exec5.c
@@ -55,10 +55,7 @@ int get_line(char s[], int lim){
      int c, i = -1;
      for(i = 0; i < lim - 1 && ((c = getchar()) != EOF) && c != '\n'; i++)
          s[i] = c;
-     /* if (c == '\n'){
-           s[i]=c;
-           i++;
-     } */
+
      s[i] = '\0';
      return i;
 }
