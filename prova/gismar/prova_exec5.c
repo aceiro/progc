c39818f0d558f6b2e1bcf6b8be2c8570792375c0
gismarb <gismar_barbosa@yahoo.com.br>
2018-10-12 22:23:55 -0300

    Prova - Exercicio 5

--- prova/gismar/prova_exec5.c
+++ prova/gismar/prova_exec5.c
@@ -1,52 +1,95 @@
 /*
-
-	Verificando se uma palavra e Palindroma
-
-*/
-
+ *  Programa verificar se uma palavra e um Palindromo: uma palavra
+ *  que se pode ler, indiferentemente, da esquerda para direita ou
+ *  vice-versa.
+ *
+ *  Codigo usou trechos do programa maxline.c (Conteudo Aula 6) com
+ *  algumas modificacoes (funcoes get_line, copy).
+ *  Tambem foi criada uma funcao (rev_str), baseada na funcao (strrev)
+ *  que nao esta disponivel para as bibliotecas nativas do gcc Unix.
+ *
+ *  Por ultimo, foi criada uma funcao (comp_str), baseada na funcao
+ *  nativa (strncmp).
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-#define TAM_MAX_WORD
+#define MAX_LINE 100
 
-void get_line(char *line, int limit){
-     int  i = 0;
-     char c = '\0';
+/* declaração das rotinas a serem usadas */
+int get_line(char line[], int maxline);
+void copy(char to[], char from[]);
+void *rev_str(char *str, int size);
+int comp_str(char str_a[], char str_b[], int size);
 
-     while( (i < limit-1) && (c = getchar()) && (c!=EOF) && (c!='\n') )
-	     line[i++] = c;
+int main(){
+     int len_a;
+     int len_b;
+     int comp_word;
+     char word[MAX_LINE];
+     char inv_word[MAX_LINE];
 
-     line[i] = '\0';
+     system("clear");
+     printf("\n\n\tVamos verificar se uma palavra e um PALINDROMO ");
+     printf("\n\n\tEntre com uma PALAVRA para que possamos verifica-la: ");
+
+     len_a = get_line(word, MAX_LINE);
+     copy(inv_word, word);
+     rev_str(inv_word, len_a);
+     len_b = len_a;
+     comp_word = comp_str(word, inv_word, len_b);
+
+     if (comp_word == 1)
+         printf("\n\n\t\tA Palavra [ %s ] e um PALINDROMO\n\n", word);
+     else
+         printf("\n\n\t\tA Palavra [ %s ] nao e um PALINDROMO\n\n", word);
+
+     return 0;
+
 }
 
-
-int main() {
-	
-	char palavra[TAM_MAX_WORD] = "\0";
-	char lixo[100];
-    size_t len = 0;
-    char *ptr_nome;
-    int aux = 0;	
-
-	printf("\n\n\tVamos verificar se uma Palarva e Palidroma"); 
-	printf("\n\n\tPalidroma: sao palavras ou frases que podem ser lidas da esquerda para a direita ou da direita para a esquerda");
-	
-	printf("\n\n\tDigite uma [ PALAVARA ]: ");
-	getline(&ptr_nome, &len, stdin);
-
-	strcpy(palavra, ptr_nome);
-	strrev(palavra);
-	aux = strcmp(ptr_nome, palavra);
-
-	if (aux == 0)
-		printf("\n\n\tA PALAVARA [ %s ] nao e [ PALINDROMA ]: ", ptr_nome);
-
-	else
-		printf("\n\n\tA PALAVARA [ %s ] e [ PALINDROMA ]: ", ptr_nome);		
-
-
-	return 0;
+int get_line(char s[], int lim){
+     int c, i = -1;
+     for(i = 0; i < lim - 1 && ((c = getchar()) != EOF) && c != '\n'; i++)
+         s[i] = c;
+     /* if (c == '\n'){
+           s[i]=c;
+           i++;
+     } */
+     s[i] = '\0';
+     return i;
 }
+
+void copy(char to[], char from[]){
+      int i = 0;
+      while((to[i] = from[i]) !='\0')
+          i++;
+}
+
+void *rev_str(char *str, int size) {
+    int i = size -1;
+    int j = 0;
+    char ch;
+    while (i > j) {
+	    ch = str[i];
+        str[i] = str[j];
+        str[j] = ch;
+        i--;
+        j++;
+    }
+    return str;
+}
+
+int comp_str(char str_a[], char str_b[], int size) {
+    int i;
+    for(i = 0; i < (size); i++) {
+        if (str_a[i] != str_b[i])
+            return 0;
+        else
+            return 1;
+    }
+}
+
