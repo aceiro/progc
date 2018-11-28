8ad7f1beaf5cf20a12e364fdd58c3bcb68efd0b5
Amanda <amandasantos@outlook.com>
2018-11-28 21:59:52 -0200

    Ex 22

--- exercicios/amanda/ex22.c
+++ exercicios/amanda/ex22.c
@@ -12,12 +12,14 @@ c) Imprima quantos são "GOL" e o percentual em relação ao total.*/
 int main(){
 	
 	int ano, quantidade_veiculos;
-	char veiculo;
+	char veiculo[15];
 	char placa[8];
+	char cor[15];
 	int aux = 0;
 	int count_veiculo = 0;
 	int count_ano = 0;
 	int count_placa = 0;
+	int count_cor = 0;
     
 
 	printf("**********************************************\n");
@@ -26,26 +28,40 @@ int main(){
 
 	printf("Digite a quantidade de veículos que teremos no Questionário: ");
 	scanf("%i", &quantidade_veiculos);
+	setbuf(stdin, NULL);
 	
 	if (quantidade_veiculos > 10){
 		printf("Limite máximo de carros atingidos! Máximo de 10 veículos\n");
 	    
 	} else {
 
-		for(int aux = 1; aux <= quantidade_veiculos; aux++) {
+		for(aux = 1; aux <= quantidade_veiculos; aux++) {
 		    printf("Qual o modelo do veículo %i?\n", aux);
-		    scanf("%s", &veiculo);
+		    scanf("%[^\n]s", veiculo);
+		    setbuf(stdin, NULL);
 		    count_veiculo++;
+
+		    printf("Qual é a cor do veículo %i?\n", aux);
+		    scanf("%[^\n]s", cor);
+		    setbuf(stdin, NULL);
+		    	if(strcmp(cor, "Verde")){
+		    		count_cor++;
+				}
 		    
-		    printf("Qual o ano de fabricação do veículo?\n");
+		    printf("Qual o ano de fabricação do veículo %i?\n", aux);
 		    scanf("%i", &ano);
 		    setbuf(stdin, NULL);
 		    count_ano++;
 		    
-		    printf("Qual é a placa do veículo?\n");
+		    printf("Qual é a placa do veículo %i?\n", aux);
 		    fgets(placa, sizeof(placa), stdin);
+		    printf("\n");
 		    count_placa++;
 		}
 	}
+
+	printf("Quantidade de verdes: %i\n", count_cor);
+
+
 	
 }
